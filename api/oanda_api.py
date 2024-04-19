989812a30064e6930b5662a646e2ae9829e25fd8
Aryan Gandhi <43526001+aryangandhi@users.noreply.github.com>
2024-04-19 16:22:18 -0400

    Add files via upload

---
+++ api/oanda_api.py
@@ -0,0 +1,116 @@
+import requests
+# import constants.defs as defs
+from constants.defs import API_KEY, ACCOUNT_ID, OANDA_URL
+import pandas as pd
+from dateutil import parser
+from datetime import datetime as dt
+
+class OandaApi:
+  
+  def __init__(self) -> None:
+    self.session = requests.Session()
+    self.session.headers.update({
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+    })
+    
+  def make_request(self, url, verb='get', code=200, params=None, data=None, headers=None):
+    full_url = f"{OANDA_URL}/{url}"
+    
+    try:
+      response = None
+      if verb == "get":
+        response = self.session.get(full_url, params=params, data=data, headers=headers)
+      
+      if not response:
+        return False, {'error': 'verb not found'}
+      
+      if response.status_code == code:
+        return True, response.json()
+      
+      else:
+        return False, response.json()
+      
+    except Exception as error:
+      return False, {'Exception': error}
+  
+  def get_account_ep(self, ep, data_key):
+    url = f"accounts/{ACCOUNT_ID}/{ep}"
+    ok, data = self.make_request(url)
+    
+    if ok and data_key in data:
+      return data[data_key]
+    else:
+      print("ERROR get_account_ep()", data)
+      return None
+  
+  def get_account_summary(self):
+    return self.get_account_ep("summary", "account")
+  
+  def get_account_instruments(self):
+    return self.get_account_ep("instruments", "instruments")
+  
+  def fetch_candles(self, pair_name, count=10, granularity="H1", price="MBA", 
+                    date_f=None, date_t=None):
+    
+    url = f"instruments/{pair_name}/candles"
+    params = dict(
+        granularity = granularity,
+        price = price
+    )
+    
+    if date_f is not None and date_t is not None:
+      date_format = "%Y-%m-%dT%H:%M:%SZ"
+      params["from"] = dt.strftime(date_f, date_format)
+      params["to"] = dt.strftime(date_t, date_format)
+    else:
+      params["count"] = count
+
+    ok, response_data = self.make_request(url, verb='get', code=200, params=params, data=None, headers=None)
+    data = response_data
+
+    if ok and 'candles' in data:
+      return data['candles']
+    else:
+      print("ERROR fetch_candles()", params, data)
+      return None
+
+  def get_candles_df(self, pair_name, **kwargs):
+  
+    data = self.fetch_candles(pair_name, **kwargs)
+    
+    if data is None:
+      return None
+    elif len(data) == 0:
+      return pd.DataFrame()
+    
+    prices = ['mid', 'bid', 'ask']
+    ohlc = ['o', 'h', 'l', 'c']
+    
+    final_data = []
+    for candle in data:
+        if candle['complete'] == False:
+            continue
+        new_dict = {}
+        new_dict['time'] = parser.parse(candle['time'])
+        new_dict['volume'] = candle['volume']
+        
+        for p in prices:
+          if p in candle:
+            for o in ohlc:
+                new_dict[f"{p}_{o}"] = float(candle[p][o])
+        final_data.append(new_dict)
+    df = pd.DataFrame.from_dict(final_data)
+    return df
+
+  def create_data_file(self, pair_name, count=10, granularity="H1"):
+    code, data = fetch_candles(pair_name, count, granularity)
+    if code != 200:
+        print("Failed", pair_name, data)
+        return
+    if len(data) == 0:
+        print("No candles", pair_name)
+        return
+    candles_df = get_candles_df(data)   
+    candles_df.to_pickle(f"../data/{pair_name}_{granularity}.pkl")
+    print(f"{pair_name} {granularity} {candles_df.shape[0]} candles")
---
+++ constants/defs.py
@@ -0,0 +1,3 @@
+API_KEY = "897e9306b5860257fe7ad40a8db63a95-46cacd7e732372c7a5bcbdcc42bec0b6"
+ACCOUNT_ID = "101-002-21354734-001"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
